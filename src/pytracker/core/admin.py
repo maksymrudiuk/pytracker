145c18ee1c69c6ad8d52e74a162ff5178cb406df
Maksym Rudiuk <maksym.rudiuk@gmail.com>
2019-07-10 18:50:41 +0300

    Added task, comment, developerinproject models and configure them in modeladmin

--- src/pytracker/core/admin.py
+++ src/pytracker/core/admin.py
@@ -1,5 +1,41 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Task, Comment, DeveloperInProject
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    fields = ('owner', 'comment', 'date_of_add')
+    readonly_fields = ('owner', 'comment', 'date_of_add')
+    can_delete = False
+    extra = 0
+
+class DeveloperInProjectInline(admin.TabularInline):
+    model = DeveloperInProject
+    verbose_name = "Developer"
+    verbose_name_plural = "Developers"
+    can_delete = False
+    extra = 0
+
+class TaskInline(admin.TabularInline):
+    model = Task
+    fields = ('topic', 'priority', 'start_date', 'end_date', 'estimated_time')
+    readonly_fields = ('topic', 'priority', 'start_date', 'end_date', 'estimated_time')
+    can_delete = False
+    extra = 0
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [
+        DeveloperInProjectInline,
+        TaskInline,
+    ]
+
+class TaskAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
 
 # Register your models here.
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Comment)
--- src/pytracker/core/models.py
+++ src/pytracker/core/models.py
@@ -21,11 +21,166 @@ class Project(models.Model):
         blank=False
     )
 
+    developers = models.ManyToManyField(
+        'user.UserProfile',
+        through='DeveloperInProject')
+
     description = tinymce_models.HTMLField(
         "Desrciption"
     )
 
     def __str__(self):
         """Unicode representation of Project."""
-        return self.name
+        return "%s" % self.name
 
+
+class Task(models.Model):
+    """Model definition for Task."""
+
+    class Meta:
+        """Meta definition for Task."""
+
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
+    TASK_TYPES = (
+        (1, 'Feature'),
+        (2, 'Bug'),
+    )
+
+    TASK_PRIORITY = (
+        (1, 'Normal'),
+        (2, 'High'),
+        (3, 'Urgently'),
+    )
+
+    topic = models.CharField(
+        "Topic",
+        max_length=255,
+        blank=False
+    )
+
+    desciption = tinymce_models.HTMLField(
+        "Desrciption"
+    )
+
+    start_date = models.DateTimeField()
+
+    end_date = models.DateTimeField()
+
+    task_type = models.SmallIntegerField(
+        "Task type",
+        choices=TASK_TYPES,
+        blank=False
+    )
+
+    priority = models.SmallIntegerField(
+        "Priority",
+        choices=TASK_PRIORITY,
+        blank=False
+    )
+
+    estimated_time = models.SmallIntegerField()
+
+    creator = models.ForeignKey(
+        'user.UserProfile',
+        related_name='user_creator',
+        null=True,
+        on_delete=models.CASCADE
+    )
+
+    performer = models.ForeignKey(
+        'user.UserProfile',
+        related_name='user_performer',
+        null=True,
+        on_delete=models.SET_NULL
+    )
+
+    project = models.ForeignKey(
+        'core.Project',
+        null=True,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        """Unicode representation of Task."""
+        return "%s" % self.topic
+
+
+class Comment(models.Model):
+    """Model definition for Comment."""
+
+    # TODO: Define fields here
+
+    class Meta:
+        """Meta definition for Comment."""
+
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+    comment = models.TextField()
+
+    owner = models.ForeignKey(
+        'user.UserProfile',
+        null=True,
+        on_delete=models.CASCADE
+    )
+
+    date_of_add = models.DateTimeField(
+        "Date of adding",
+        auto_now_add=True
+    )
+
+    for_task = models.ForeignKey(
+        'core.Task',
+        null=True,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        """Unicode representation of Comment."""
+        return "%s-%s" % (self.owner, self.date_of_add)
+
+
+class DeveloperInProject(models.Model):
+    """Model definition for DeveloperInProject."""
+
+
+    class Meta:
+        """Meta definition for DeveloperInProject."""
+
+        verbose_name = 'Developer In Project'
+        verbose_name_plural = 'Developer In Projects'
+
+    developer = models.ForeignKey(
+        'user.UserProfile',
+        null=True,
+        on_delete=models.SET_NULL
+    )
+
+    project = models.ForeignKey(
+        'core.Project',
+        null=True,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        """Unicode representation of DeveloperInProject."""
+        return "%s - %s" % (self.developer.username, self.project.name)
+
+
+class TimeLogging(models.Model):
+    """Model definition for TimeLogging."""
+
+
+    class Meta:
+        """Meta definition for TimeLogging."""
+
+        verbose_name = 'TimeLogging'
+        verbose_name_plural = 'TimeLoggings'
+
+    spent_time = models.SmallIntegerField()
+
+    def __str__(self):
+        """Unicode representation of TimeLogging."""
+        pass
