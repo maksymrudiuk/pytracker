6785be8ea41321e095861eea148364999c5ca165
Maksym Rudiuk <maksym.rudiuk@gmail.com>
2019-07-10 16:03:11 +0300

    Add project model in core app. Install django-tinymce and use in project model

--- src/pytracker/core/models.py
+++ src/pytracker/core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from tinymce import models as tinymce_models
 
 # Create your models here.
+class Project(models.Model):
+    """Model definition for Project."""
+
+    class Meta:
+        """Meta definition for Project."""
+
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+
+    slug_id = models.SlugField(
+        "Unique string id"
+    )
+
+    name = models.CharField(
+        "Name",
+        max_length=200,
+        blank=False
+    )
+
+    description = tinymce_models.HTMLField(
+        "Desrciption"
+    )
+
+    def __str__(self):
+        """Unicode representation of Project."""
+        return self.name
+
