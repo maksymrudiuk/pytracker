12faec9836cf11b4ac36bc0f38e08ff1c457a1bb
Maksym Rudiuk <maksym.rudiuk@gmail.com>
2019-07-10 15:43:34 +0300

    Create custum user model

--- src/pytracker/user/admin.py
+++ src/pytracker/user/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import UserProfile
+
 
 # Register your models here.
+class UsersInline(admin.TabularInline):
+    model = UserProfile
+    fields = ('first_name', 'last_name', 'position', 'username')
+    extra = 0
+
+
+class UserProfileAdmin(UserAdmin):
+
+    fieldsets = (
+        ("General",
+            {'fields': ('username', 'password')}
+        ),
+        ("Personal",
+            {'fields':
+                ('first_name',
+                 'last_name',
+                 'date_of_birth',
+                 'position',
+                 'photo'
+                )}),
+        ("Permissions",
+            {'fields':
+                ('is_active',
+                 'is_staff',
+                 'is_superuser',
+                 'groups',
+                 'user_permissions',
+                 )}),
+        ("Important dates",
+            {'fields':
+                ('last_login',
+                'date_joined')}),
+    )
+
+admin.site.register(UserProfile, UserProfileAdmin)
--- src/pytracker/user/models.py
+++ src/pytracker/user/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group
 
 # Create your models here.
+class UserProfile(AbstractUser):
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    POSITIONS = (
+        (1, 'Admin'),
+        (2, 'Developer'),
+    )
+
+    first_name = models.CharField(
+        "First name",
+        max_length=100,
+        blank=False
+    )
+
+    last_name = models.CharField(
+        "Last name",
+        max_length=100,
+        blank=False
+    )
+
+    date_of_birth = models.DateField(
+        "Date of birth",
+        null=True
+    )
+
+    position = models.SmallIntegerField(
+        "Position",
+        choices=POSITIONS,
+        null=True,
+        blank=False
+    )
+
+    photo = models.ImageField(
+        "User photo",
+        blank=True
+    )
+
+    def get_short_name(self):
+        return self.username
+
+    def __str__(self):
+        return self.get_short_name()
+
+    @property
+    def isAdmin(self):
+        return self.position == 1
+
+    @property
+    def isDeveloper(self):
+        return self.position == 2
