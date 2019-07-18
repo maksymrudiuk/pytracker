63be0bd7da7b7332cd71fd51945b9d04f776e8eb
Maksym Rudiuk <maksym.rudiuk@gmail.com>
2019-07-18 08:01:56 +0300

    Migrate on MySQL(MariaDB)

--- src/pytracker/pytracker/settings.py
+++ src/pytracker/pytracker/settings.py
@@ -78,12 +78,16 @@ WSGI_APPLICATION = 'pytracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+try:
+    from .db_settings import DATABASES
+
+except:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
