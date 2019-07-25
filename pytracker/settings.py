c920ca2f55eb9b79f5b4a9b9715bf32e80123553
Maksym Rudiuk <maksym.rudiuk@gmail.com>
2019-07-25 21:43:40 +0300

    Activated boto3 S3 storage

--- pytracker/settings.py
+++ pytracker/settings.py
@@ -148,8 +148,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static')
 ]
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 # Media configuration
 MEDIA_URL = '/media/'
